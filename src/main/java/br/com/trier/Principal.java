b152e3cacdc1076e63057e329c4b13450d1d4413
SorayaYF <sorayayasin@hotmail.com>
2023-05-22 15:50:35 -0300

    Segundo commit

--- src/main/java/br/com/trier/Principal.java
+++ src/main/java/br/com/trier/Principal.java
@@ -3,7 +3,8 @@ package br.com.trier;
 public class Principal {
 
 	public static void main(String[] args) {
-		System.out.println("Olá mundo!");
+		System.out.println("Olá Mundão!!");
+		System.out.println("*******************");
 
 	}
 
